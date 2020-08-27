2691a6700d345b0b93737f2e5315055a28ddf862
Erwan TILLY <erwan.tilly@niji.fr>
2020-08-27 10:31:38 +0200

    init pbxproj RunKitDemo

---
+++ RunKitDemo/RunKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RunKitDemo
+//
+//  Created by Erwan TILLY on 27/08/2020.
+//  Copyright © 2020 Erwan TILLY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
